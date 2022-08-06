4d08f3c4ecf2b1881d1a41f82b25822b74c902f1
Franco Baigorria <baigorriafrancoad@outlook.com>
2022-08-06 15:57:50 -0300

    Added: custom primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{js,jsx,ts,tsx}"],
-	theme: {
-		extend: {
-			borderRadius: {
-				xl: "24px",
-			},
-		},
-	},
-	plugins: [],
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        extend: {
+            borderRadius: {
+                xl: "24px",
+            },
+            colors: {
+                primary: "#344154",
+                "primary-hover": "#222b38",
+            },
+        },
+    },
+    plugins: [],
 };
