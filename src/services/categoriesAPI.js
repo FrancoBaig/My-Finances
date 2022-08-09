63ced37895ab706dfbfdb998b6db3ea58fbb659f
Franco Baigorria <baigorriafrancoad@outlook.com>
2022-08-09 18:10:12 -0300

    Improved: simplified category retrieval service

--- src/services/categoriesAPI.js
+++ src/services/categoriesAPI.js
@@ -2,14 +2,11 @@ import axios from "axios";
 
 const baseURL = process.env.REACT_APP_API_BASE_URL;
 
-export const getCategoriesService = async (userId, token) => {
+export const getCategoriesService = async (token) => {
     try {
-        const response = await axios.get(
-            `${baseURL}/api/categories/${userId}`,
-            {
-                headers: { Authorization: token },
-            }
-        );
+        const response = await axios.get(`${baseURL}/api/categories`, {
+            headers: { Authorization: token },
+        });
         return response.data;
     } catch (err) {
         console.log(err);
