b5ede4d19373c862e2be2f7a3b37aee325a0cffe
Franco Baigorria <baigorriafrancoad@outlook.com>
2022-08-10 16:59:12 -0300

    Added: validate transaction form
    validate the transaction form with react-hook-form

--- src/components/home/Income.js
+++ src/components/home/Income.js
@@ -2,11 +2,11 @@ import React, { useState, useEffect } from "react";
 
 // Components
 import InputDatePicker from "./InputDatePicker";
-import DropDown from "./DropDown";
 
 // heruicons
 import { CurrencyDollarIcon } from "@heroicons/react/outline";
 import { XIcon } from "@heroicons/react/solid";
+import { ChevronDownIcon } from "@heroicons/react/solid";
 
 // services
 import { getCategoriesService } from "../../services/categoriesAPI";
@@ -19,13 +19,20 @@ import { createCategories } from "../../redux/states/categoriesState";
 // Moment
 import moment from "moment";
 
+// useForm
+import { useForm } from "react-hook-form";
+
 function Income({ setModal }) {
     const user = useSelector((store) => store.user);
+    const items = useSelector((state) => state.categories);
     const dispatch = useDispatch();
-    const [description, setDescription] = useState("");
-    const [categoryId, setCategoryId] = useState(null);
-    const [amount, setAmount] = useState(0);
-    const [date, setDate] = useState(new Date());
+
+    const {
+        register,
+        handleSubmit,
+        control,
+        formState: { errors },
+    } = useForm();
 
     useEffect(() => {
         const getCategories = async (token) => {
@@ -35,17 +42,16 @@ function Income({ setModal }) {
         getCategories(user.token);
     }, [dispatch, user]);
 
-    const handleSubmitTransaction = async (e) => {
-        e.preventDefault();
-
+    const handleSubmitTransaction = (form) => {
         const data = {
-            description,
-            categoryId,
-            amount: parseInt(amount),
-            date: moment(date).format("YYYY-MM-DD HH:mm:ss"),
+            description: form.description,
+            categoryId: parseInt(form.category),
+            amount: parseInt(form.amount),
+            date: moment(form.date).format("YYYY-MM-DD HH:mm:ss"),
         };
 
         postTransactionService(data, user.token);
+
         setModal(false);
     };
 
@@ -67,29 +73,70 @@ function Income({ setModal }) {
                 </button>
             </div>
             <form
-                onSubmit={handleSubmitTransaction}
+                onSubmit={handleSubmit(handleSubmitTransaction)}
                 className="grid gap-6 px-3 pt-4"
                 id="transaction-form"
             >
-                <input
-                    type="text"
-                    placeholder="Description..."
-                    className="input-transaction"
-                    onChange={({ target }) => setDescription(target.value)}
-                />
+                <div>
+                    <label className="label">Description</label>
+                    <input
+                        type="text"
+                        className="input-transaction"
+                        {...register("description", { required: true })}
+                    />
+                    {errors.description && (
+                        <span className="input-error">
+                            The description is required
+                        </span>
+                    )}
+                </div>
+
                 <div className="relative">
+                    <label className="label">Amount</label>
                     <CurrencyDollarIcon className="h-6 w-6 absolute text-gray-400" />
                     <input
-                        type="number"
                         className="input-transaction w-6/12 pl-7"
-                        onChange={({ target }) => setAmount(target.value)}
+                        {...register("amount", {
+                            required: true,
+                            pattern: /^[0-9]*$/,
+                        })}
                     />
+                    {errors.amount && errors.amount.type === "required" && (
+                        <span className="input-error">
+                            The amount is required
+                        </span>
+                    )}
+                    {errors.amount && errors.amount.type === "pattern" && (
+                        <span className="input-error">
+                            Amount must be a number
+                        </span>
+                    )}
                 </div>
-                <InputDatePicker date={date} setDate={setDate} />
-                <DropDown
-                    categoryId={categoryId}
-                    setCategoryId={setCategoryId}
-                />
+
+                <div>
+                    <label className="label mb-2">Due date</label>
+                    <InputDatePicker control={control} errors={errors} />
+                </div>
+
+                <div>
+                    <label className="label">Category</label>
+                    <div className="relative">
+                        <select
+                            className="block w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                            id="grid-state"
+                            {...register("category", {
+                                valueAsNumber: true,
+                            })}
+                        >
+                            {items.map((item) => (
+                                <option key={item.id} value={item.id}>
+                                    {item.title}
+                                </option>
+                            ))}
+                        </select>
+                        <ChevronDownIcon className="h-4 w-4 pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700" />
+                    </div>
+                </div>
             </form>
         </div>
     );
--- src/components/home/InputDatePicker.js
+++ src/components/home/InputDatePicker.js
@@ -2,33 +2,39 @@ import React, { forwardRef } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
-function InputDatePicker({ date, setDate }) {
+// Icon
+import { CalendarIcon } from "@heroicons/react/outline";
+
+// react-hook-form
+import { Controller } from "react-hook-form";
+
+function InputDatePicker({ control, errors }) {
     const ExampleCustomInput = forwardRef(({ value, onClick }, ref) => (
         <div onClick={onClick} ref={ref} className="relative">
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-6 w-6 absolute text-gray-400"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth="2"
-            >
-                <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
-                />
-            </svg>
+            <CalendarIcon className="w-5 text-gray-400 absolute bottom-2 left-0" />
             <div className="input-transaction pl-7 w-4/12">{value}</div>
         </div>
     ));
 
     return (
-        <DatePicker
-            selected={date}
-            onChange={(date) => setDate(date)}
-            customInput={<ExampleCustomInput />}
-        />
+        <>
+            <Controller
+                control={control}
+                name="date"
+                rules={{ required: true }}
+                render={({ field }) => (
+                    <DatePicker
+                        placeholder="Select a date..."
+                        onChange={(date) => field.onChange(date)}
+                        selected={field.value}
+                        customInput={<ExampleCustomInput />}
+                    />
+                )}
+            />
+            {errors.date && (
+                <span className="input-error">This field is required</span>
+            )}
+        </>
     );
 }
 
