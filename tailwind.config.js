31dd77d886ead861080c03a5d9397df92e387a54
Franco Baigorria <baigorriafrancoad@outlook.com>
2022-08-10 14:00:56 -0300

    Added: font family "Inter var"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+
+// tailwind.config.js
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
         extend: {
+            fontFamily: {
+                sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+            },
             borderRadius: {
                 xl: "24px",
             },
